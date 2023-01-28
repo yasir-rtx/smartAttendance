d1d419d2065bc3cec3c6eda836a92f854b9d6c4c
yasir <yelfvankarlenstain@gmail.com>
2023-01-28 14:17:15 +0700

    Update Main.py
    Load data and basic configuration

--- Main.py
+++ Main.py
@@ -39,7 +39,52 @@ from tkinter import ttk
 ###############################################################################################################################
 #                                                    ===== FUNCTIONS =====                                                    #
 ###############################################################################################################################
-# Code Here
+# Generate file absensi.json
+def generateAbsensiFile(data):
+    print("Generate file : absensi.json")
+    with open(absensi_path, "w") as absensi:
+        json.dump(data, absensi, indent=4)
+    print("File absensi is created\n")
+    #########################################
+    
+# Generate object key(date) into absensi.json
+def generateKeyDate(date):
+    print(f"Generate key({date})")
+    with open(absensi_path, "r") as absensi:
+        data = json.load(absensi)
+        data.update({date:[]})
+        json.dump(data, open(absensi_path, "w"), indent=4)
+    print(f"Key({date}) for today is created\n")
+###########################################################
+
+# Generate objects key(list mahasiswa) into object key(date)
+def generateListMahasiswa(date):
+    print("Generate Mahasiswa Key")
+    with open(mahasiswa_path, "r") as file:
+        mahasiswa = json.load(file)
+        mahasiswaValue = mahasiswa.values()
+        # Generate mahasiswa key into date object in absensi.json
+        for value in mahasiswaValue:
+            # store nobp value into temporary variable
+            nobp = value[0]["nobp"]
+            print(f"Generate Mahasiswa Key for {nobp}")
+            with open(absensi_path, "r") as absensi:
+                # Generate mahasiswa value into date object in absensi.json
+                y = {nobp:{
+                    "nobp": nobp,
+                    "nama": value[0]["nama"],
+                    "date": date,
+                    "time": "-",
+                    "ket": "Tidak Hadir"
+                }}
+                data = json.load(absensi)
+                # metode dump
+                temp = data[date]   # akses object hari ini
+                temp.append(y)
+                json.dump(data, open(absensi_path, "w"), indent=4)
+                print(f"Key({nobp}) is generated into object({date})\n")
+    print("Mahasiswa Key is generated\n")
+########################################################################
 ###############################################################################################################################
 
 
@@ -56,11 +101,69 @@ print("Load FaceNet model & HaarCascade file\n")
 # MyFaceNet = load_model("Model\\facenet_keras.h5")
 # HaarCascade = cv2.CascadeClassifier(cv2.samples.findFile(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'))
 
+# Directory Data
+dataPath = "Data\\"
+if not exists(dataPath):
+    mkdir(dataPath)
+    print("Directory Data is created")
+
+# Dataset Path
+datasetPath = dataPath + "Dataset\\"
+if not exists(datasetPath):
+    mkdir(datasetPath)
+    print("Directory Dataset is created")
+
 # Training Face Path
-savePath = "Data\\Dataset\\train"
+savePath = datasetPath + "train\\"
 if not exists(savePath):
     mkdir(savePath)
-
+    print("Directory Data Training is created")
+    
+# Testing Face Path
+trainPath = datasetPath + "test\\"
+if not exists(trainPath):
+    mkdir(trainPath)
+    print("Directory Data Testing is created")
+    
+# Face Signature
+signature_path = "Data\\signature.pkl"
+if exists(signature_path):
+    mySignature = open("Data\\signature.pkl", "rb")
+    faceDatabase = pickle.load(mySignature)
+    mySignature.close()
+else:
+    faceDatabase = {}
+    
+# Mahasiswa
+mahasiswa_path = "Data\\mahasiswa.json"
+if not exists(mahasiswa_path):
+    print("Generate File : mahasiswa.json")
+    data = {}
+    with open(mahasiswa_path, "w") as mahasiswa:
+        json.dump(data, mahasiswa, indent=4)
+        print("File mahasiswa.json is creasted\n")
+        
+# Absensi
+absensi_path = "Data\\absensi.json"
+# Prepare absensi.json
+if not exists(absensi_path):                                    # create absensi.json
+    generateAbsensiFile({})
+    generateKeyDate(date)
+    generateListMahasiswa(date)
+else:                                                           # if absensi.json has been created
+    # check if date object is already made or not
+    finding = 0
+    with open(absensi_path, "r") as absensi:
+        data = json.load(absensi)
+        dates = data.keys()
+        for dateFound in dates:
+            if dateFound == date:
+                finding = 1
+    if finding == 1:                                            # prevent object(date) from being reset
+        print("File absensi.json is ready")
+    else:                                                       # generate object(date)
+        generateKeyDate(date)
+        generateListMahasiswa(date)
 ###############################################################################################################################
 
 
